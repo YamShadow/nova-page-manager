b49f21deec338e40b48510d8cd60d7027a22cc5f
Mathieu YamiShadow <yamishadowpro@gmail.com>
2020-05-19 15:32:13 +0200

    Update Region for integrate model from Spatie Images

--- src/Models/Region.php
+++ src/Models/Region.php
@@ -3,9 +3,13 @@
 namespace OptimistDigital\NovaPageManager\Models;
 
 use OptimistDigital\NovaPageManager\NovaPageManager;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Region extends TemplateModel
+class Region extends TemplateModel implements HasMedia
 {
+    use InteractsWithMedia;
+    
     public function __construct(array $attributes = [])
     {
         parent::__construct($attributes);
